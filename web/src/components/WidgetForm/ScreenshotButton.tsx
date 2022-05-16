4aa41db67df429e5f52cdc0c1986105b94b29c96
Adam Martins Santos <103488271+adammartinss@users.noreply.github.com>
2022-05-16 17:51:35 -0300

    sa

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -45,7 +45,7 @@ export default function ScreenshotButton({
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
         >
             
-            {isTakingScreenshot ? <Loading/>: <Camera className='w-6 h-6'/>}
+            {isTakingScreenshot ? <Loading/> : <Camera className='w-6 h-6'/>}
         </button>
     );
 }
